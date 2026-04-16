ca416c9bcad858314c9c5529fc9581608ee81a3a
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-04-16 10:34:51 +0200

    Bursdag system fungerer, må jobbe på verifikasjon

--- events/birthdayEvent.js
+++ events/birthdayEvent.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 const cron = require('node-cron');
 let birthdayData;
@@ -6,26 +6,43 @@ let tempDate;
 let channel;
 
 
-const channelId = "1493182992263417856";
-const filePath = path.resolve(__dirname, "../data/birthdays.json");
+const channelId = "1493182992263417856"; // currently testchannel in abarkiv
+const filePath = path.resolve(__dirname, "../data/private/birthdays.json");
 
-async function loadBirthdays(client, filePath) {
-    if (!fs.existsSync(filePath)) {
-        console.error("File does not exist");
-        return;
-    }
-
-    const fileData = fs.readFileSync(filePath, "utf-8");
-    let data;
-
+async function checkFileExists() {
     try {
-        data = JSON.parse(fileData);
-    } catch (e) {
-        console.error("Could not parse file", e);
-        return;
-    }
+        await fs.mkdir(path.dirname(filePath), { recursive: true });
 
-    birthdayData = data;
+
+        try {
+            await fs.access(filePath);
+        } catch {
+            await fs.writeFile(
+                filePath,
+                JSON.stringify(
+                    {
+                        users: {},
+                        dates: {}
+                    },
+                    null,
+                    2
+                ),
+                "utf-8"
+            );
+        }
+    } catch (err) {
+        console.error("Error ensuring file exists:", err);
+    }
+}
+
+async function loadBirthdays() {
+    try {
+        await checkFileExists();
+        birthdayData = JSON.parse(await fs.readFile(filePath, "utf-8"));
+    } catch (err) {
+        console.error("Error loading birthdays:", err);
+        birthdayData = {};
+    }
 }
 
 async function getData() {
@@ -44,23 +61,21 @@ async function setData(birthdayData2) {
 async function saveBirthdays() {
 
     try {
-        await writeJSONToFile(filePath, JSON.stringify(birthdayData, null, 2));
-        alert("File saved successfully!");
-    } catch (error) {
-        console.error("Error saving file:", error);
-    }
-
-
+        fs.writeFile(filePath, JSON.stringify(birthdayData))
+    } catch (err) {
+        console.error("Error saving file:", err);
+    };
 }
 
-cron.schedule('30 16 * * *', () => {
+cron.schedule('59 9 * * *', () => {
     try {
         console.log('Running daily birthday check');
         tempDate = new Date();
-        birthdayData[
-            String(tempDate.getDate).padStart(2, '0') +
-            String(tempDate.getMonth).padStart(2, '0')
-        ].array.forEach(element => {
+
+        const list = birthdayData.dates[
+            String(tempDate.getMonth() + 1).padStart(2, '0') +
+            String(tempDate.getDate()).padStart(2, '0')] || [];
+        list.forEach(element => {
             channel.send(`Gratulerer med dagen <@${element}>`);
         })
     } catch (error) {
