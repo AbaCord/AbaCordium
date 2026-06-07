420451160a311d5cb8ac6c9cfcde3a7a6ff0cd8b
Sofus Lind <43791049+sofuslind@users.noreply.github.com>
2026-06-07 23:40:07 +0200

    refactor(events): isolate cron jobs from event modules to avoid execution during command deployment

---
+++ events/birthdayCron.js
@@ -0,0 +1,40 @@
+const fs = require("fs").promises;
+const path = require("path");
+const cron = require("node-cron");
+const { getData } = require("./birthdayEvent.js");
+
+const channelId = "1493182992263417856"; // currently testchannel in abarkiv
+
+let tempDate;
+let channel;
+
+cron.schedule("11 11 * * *", () => {
+  try {
+    console.log("Running daily birthday check");
+    tempDate = new Date();
+
+    const list =
+      getData().dates[
+        String(tempDate.getMonth() + 1).padStart(2, "0") +
+          String(tempDate.getDate()).padStart(2, "0")
+      ] || [];
+    list.forEach((element) => {
+      channel.send(`Gratulerer med dagen <@${element}>`);
+    });
+  } catch (error) {
+    console.error("Error with daily bd check:", error);
+  }
+});
+
+module.exports = {
+  name: "clientReady",
+  once: true,
+  async execute(client) {
+    try {
+      channel = await client.channels.fetch(channelId).catch(() => null);
+      console.log(`Cronjob for birthdays loaded`);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
